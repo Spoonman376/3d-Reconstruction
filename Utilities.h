4b0bbeaa9c0ce16388e3196b48d5f826104b94b4
Scott <scott@Sonnys-Mac-mini.local>
2017-05-11 08:29:05 -0600

    Added it back in, as a mac seems to need it.

--- Utilities.h
+++ Utilities.h
@@ -7,7 +7,12 @@
 #include <iostream>
 #include <stdio.h>
 #include <GLFW/glfw3.h>
-//#include <OpenGL/gl3.h>
+
+#ifdef __APPLE__
+	#include <OpenGL/gl3.h>
+#endif
+
+//A thing I use to allow CMAKE to build without interrupting your build system.
 #ifdef CMAKE
 	#include <OpenNI.h>
 #else
